f87f131341394a06a72a1c685a3aaf81ab8acaf8
echochio <chio-nzgft@users.noreply.github.com>
2017-03-10 15:20:26 +0800

    Update launch.sh

--- helper_scripts/launch.sh
+++ helper_scripts/launch.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+SETSERVER="$Server"
 
 start()
 {
         if [ ! -n "$IP" ]; then
-        	export IP=`ip addr |grep "scope global eth0"|sed 's/    inet //g'|sed 's/\/16 scope global eth0//g'`
+                export IP=`ip addr |grep "scope global eth0"|sed 's/    inet //g'|sed 's/\/16 scope global eth0//g'`
                 chmod 0600 /etc/cluster/fence_xvm.key
         fi
 
-	if [ -n "$Server" ];then
-	        cp /etc/cluster/fence_virt.conf /etc/fence_virt.conf
-	fi
-
-        /usr/sbin/fence_virtd -w
-
+        if [ -n "$SETSERVER" ];then
+                cp /etc/cluster/fence_virt.conf /etc/fence_virt.conf
+                mkdir -p /etc/systemd/system-preset/
+                echo 'enable fence_virtd.service' > /etc/systemd/system-preset/00-fence_virtd.preset
+                systemctl enable fence_virtd
+                /usr/lib/systemd/systemd --system &
+        fi
 }
 
 start
