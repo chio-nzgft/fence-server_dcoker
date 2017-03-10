ba950c1cf43ad46353b6a121b9d0ef6cb6928b49
echochio <chio-nzgft@users.noreply.github.com>
2017-03-10 10:47:19 +0800

    Update pcmk_launch.sh

--- helper_scripts/pcmk_launch.sh
+++ helper_scripts/pcmk_launch.sh
@@ -4,33 +4,12 @@ start()
 {
         if [ ! -n "$IP" ]; then
                 export IP=`ip addr |grep "scope global eth0"|sed 's/    inet //g'|sed 's/\/16 scope global eth0//g'`
-                sed -i 's/127.0.0.1/'$(echo $IP)'/g' /etc/corosync/corosync.conf
-                sed -i 's/Listen 80/Listen '$(echo $IP)':80/g' /etc/httpd/conf/httpd.conf
-                mkdir -p /etc/systemd/system-preset/
-                echo 'enable pcsd.service' > /etc/systemd/system-preset/00-pcsd.preset
-                systemctl enable pcsd
+                chmod 0600 /etc/cluster/fence_xvm.key
         fi
 
-        rm -f /usr/local/apache2/logs/httpd.pid
+        cp /etc/cluster/fence_virt.conf /etc/fence_virt.conf
+        /usr/sbin/fence_virtd -w
 
-        /sbin/httpd -DFOREGROUND &
-        /usr/lib/systemd/systemd --system &
-
-        sleep 30
-
-        /usr/share/corosync/corosync start > /dev/null 2>&1
-        mkdir -p /var/run
-
-        export PCMK_debugfile=/var/log/pacemaker.log
-        (pacemakerd &) & > /dev/null 2>&1
-        sleep 5
-
-        pid=$(pidof pacemakerd)
-        if [ "$?" -ne 0 ]; then
-                echo "startup of pacemaker failed"
-                exit 1
-        fi
-        echo "$pid" > /var/run/pacemakerd.pid
 }
 
 start
